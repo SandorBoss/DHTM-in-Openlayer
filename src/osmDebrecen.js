bf0edfd0f80b0630624922d03c2aac976a777645
SandorBoss <47780130+SandorBoss@users.noreply.github.com>
2019-06-06 11:34:47 +0200

    lehetséges megoldás a navigáció lehatárolására
    A 'dontLeaveDebrecen' nevű függvény egy vicces hibaüzenettel irányítja vissza a felhasználót a térkép kezdő pozíciójába. A viccet félretéve: minden térképmozgatás végén megvizsgáljuk a nézet középpontját és visszaállítjuk, ha az túlfut a megjeleníthető tartalom szélső koordinátáin. Ennek az a gyakorlati haszna, hogy a felhasználó által forgalmazott térképi adatokat például így is le tudjuk korlátozni.
    Van erre egy másik, elegánsabb és elterjedtebb megoldás is, amikor egy 'extent'-et metszünk ki a rétegből és a navigáció csupán ezen belül lehetséges. A fönt leírt, általam használt eljárás azért lehet célravezetőbb, mert a megjeleníthető tartalom a kiválasztott dátum függvényében szélesebb vagy szűkebb területet fedhet le. Következésképpen a két eljárás közül azt kell választanunk, amellyel egyszerűbben tudjuk követni a tartalom területváltozásait.

--- src/osmDebrecen.js
+++ src/osmDebrecen.js
@@ -1,3 +1,8 @@
+var startView = new ol.View({
+    center: ol.proj.fromLonLat([21.63079, 47.53013]),
+    zoom: 12
+  });
+
 var map = new ol.Map({
   target: 'debrecen',
   layers: [
@@ -5,8 +10,22 @@ var map = new ol.Map({
       source: new ol.source.OSM()
     })
   ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([21.63079, 47.53013]),
-    zoom: 12
-  })
+  view: startView
 });
+
+function dontLeaveDebrecen(evt) {
+  var map = evt.map;
+  var extent = map.getView().calculateExtent(map.getSize());
+  var viewCenter = ol.proj.toLonLat(ol.extent.getCenter(extent));
+  if (
+    viewCenter[0] < 21.4633 || 
+    viewCenter[0] > 21.8360 || 
+    viewCenter[1] > 47.6534 || 
+    viewCenter[1] < 47.4554
+  ) {
+    alert("Csak Debrecenben lehetsz boldog!");
+    startView.setCenter(ol.proj.fromLonLat([21.63079, 47.53013]));
+  }
+}
+
+map.on('moveend', dontLeaveDebrecen);

b6e26c6fe65436d19423b022438374e42c43ec03
Herczeg Balázs <51175974+Herbal90@users.noreply.github.com>
2019-06-06 18:23:00 +0200

    Beviteli mezők hozzáadása
    form, fieldsetek

--- resources/index.php
+++ resources/index.php
@@ -20,5 +20,44 @@
  </head>
  <body onload="init()">
    <div style="width:100%; height:100%" id="terkep"></div>
+   <form>
+    <fieldset id="day">
+      <label>Szűrés egy napra</label>
+      <p>
+        <label for="">Kért nap:</label>
+        <input type="text" value="2019-06-01" id="day" size="10" maxlength="10">
+      </p>
+      <p class="buttonParagraph">
+        <input type="button" value="Mehet">
+        <input type="reset">
+      </p>
+    </fieldset>
+    <fieldset id="betweenDays">
+      <label>Szűrés időtartamra</label>
+      <p>
+        <label for="fromDay">Első nap:</label>
+        <input type="text" value="2019-01-01" id="fromDay" size="10" maxlength="10">
+      </p>
+      <p>
+        <label for="toDay">Utolsó nap:</label>
+        <input type="text" value="2019-06-01" id="toDay" size="10" maxlength="10">
+      </p>
+      <p class="buttonParagraph">
+        <input type="button" value="Mehet">
+        <input type="reset">
+      </p>
+    </fieldset>
+    <fieldset id="line">
+      <label>Szűrés járatra</label>
+      <p>
+        <label for="line">Járatszám:</label>
+        <input type="text" value="" id="line" size="10" maxlength="10">
+      </p>
+      <p class="buttonParagraph">
+        <input type="button" value="Mehet">
+        <input type="reset">
+      </p>
+    </fieldset>
+    </form>
  </body>
 </html>

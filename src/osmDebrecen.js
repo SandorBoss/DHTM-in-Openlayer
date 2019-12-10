2f119a39460ffaf5a81e7b21620e3d2627f73b33
Böszörményi Sándor <boszormenyisandor@lltk.local>
2019-12-10 11:19:08 +0100

    turistautak.hu térképrétegémnek bekötése

--- src/osmDebrecen.js
+++ src/osmDebrecen.js
@@ -1,26 +1,26 @@
 // this is the main engine
 
 var startView = new ol.View({
-      center: ol.proj.fromLonLat([21.63079, 47.53013]),
-      zoom: 12
+    center: ol.proj.fromLonLat([21.63079, 47.53013]),
+    zoom: 12
 });
 
 var geojsonUrl = './geojson/d_line.geojson';
 
 var vill = new ol.style.Style({
-       stroke: new ol.style.Stroke({ color: [255,192,0,.75], width: 3 })
+    stroke: new ol.style.Stroke({ color: [255,192,0,.75], width: 3 })
 });
 
 var troli = new ol.style.Style({
-       stroke: new ol.style.Stroke({ color: [192,0,0,.75], width: 3 })
+    stroke: new ol.style.Stroke({ color: [192,0,0,.75], width: 3 })
 });
 
 var transportLines = new ol.layer.Vector({
-      source: new ol.source.Vector({
-          format: new ol.format.GeoJSON(),
-          url: geojsonUrl
-      }),
-      style: vill
+    source: new ol.source.Vector({
+        format: new ol.format.GeoJSON(),
+        url: geojsonUrl
+    }),
+    style: vill
 });
 
 var tileLayer = new ol.layer.Tile();
@@ -28,57 +28,70 @@ var tileLayer = new ol.layer.Tile();
 var osm = new ol.source.OSM();
 
 var satellite = new ol.source.XYZ({
-      url: 'http://www.google.hu/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
+    url: 'http://www.google.hu/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
   });
+  
+var turista = new ol.source.XYZ({
+	url: 'http://{a-d}.map.turistautak.hu/tiles/turistautak/{z}/{x}/{y}.png'
+});
 
 var debrecenXYZ = new ol.source.XYZ({
-  url: './Debrecen-XYZ/{z}/{x}/{y}.png'
+	url: './Debrecen-XYZ/{z}/{x}/{y}.png'
 });
 
 var katonai = new ol.layer.Tile({
-      source: null,
-      opacity: 0.6
+    source: null,
+    opacity: 0.6
 });
 
 var map = new ol.Map({
-  target: 'debrecen',
-  layers: [tileLayer, katonai, transportLines],
-  view: startView
+	target: 'debrecen',
+	layers: [tileLayer, katonai, transportLines],
+	view: startView
 });
 
 function dontLeaveDebrecen(evt) {
-  var map = evt.map;
-  var extent = map.getView().calculateExtent(map.getSize());
-  var viewCenter = ol.proj.toLonLat(ol.extent.getCenter(extent));
-  if (
-    viewCenter[0] < 21.4633 || 
-    viewCenter[0] > 21.8360 || 
-    viewCenter[1] < 47.4554 ||
-    viewCenter[1] > 47.6534    
-  ) {
-    startView.setCenter(ol.proj.fromLonLat([21.63079, 47.53013]));
-    alert("Csak Debrecenben lehetsz boldog!");
-  }
+	var map = evt.map;
+	var extent = map.getView().calculateExtent(map.getSize());
+	var viewCenter = ol.proj.toLonLat(ol.extent.getCenter(extent));
+	if (
+		viewCenter[0] < 21.4633 || 
+		viewCenter[0] > 21.8360 || 
+		viewCenter[1] < 47.4554 ||
+		viewCenter[1] > 47.6534    
+	) {
+		startView.setCenter(ol.proj.fromLonLat([21.63079, 47.53013]));
+		alert("Csak Debrecenben lehetsz boldog!");
+	}
 }
 
 map.on('moveend', dontLeaveDebrecen);
 
 $(document).ready(function(){
-  tileLayer.setSource(osm);
-  $("#layerSwitch").click(function(){
-      if (tileLayer.getSource() === osm) {
-        tileLayer.setSource(satellite);
-        $("#layerSwitch").val("Válts utcatérképre!");
-      } else {
-        tileLayer.setSource(osm);
-        $("#layerSwitch").val("Válts műholdra!");
-      }
-  });
-  $("#extraLayer").click(function(){
-    if (katonai.getSource()) {
-      katonai.setSource(null);
-    } else {
-      katonai.setSource(debrecenXYZ);
-    }
-  });
+	tileLayer.setSource(osm);
+	$("#layerSwitch").click(function(){
+		switch (tileLayer.getSource()) {
+        case osm:
+            tileLayer.setSource(satellite);
+            $("#layerSwitch").val("VĂĄlts tĂşristatĂŠrkĂŠpre!");
+            break;
+        case satellite:
+            tileLayer.setSource(turista);
+            $("#layerSwitch").val("VĂĄlts utcatĂŠrkĂŠpre!");
+            break;
+        case turista:
+            tileLayer.setSource(osm);
+            $("#layerSwitch").val("VĂĄlts mĹąholdra!");
+            break;
+        default:
+            break;
+		}
+	});
+	$("#extraLayer").click(function(){
+		if (katonai.getSource()) {
+			katonai.setSource(null);
+		} else {
+			katonai.setSource(debrecenXYZ);
+		}
+	});
 });
